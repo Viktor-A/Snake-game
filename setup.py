9e779bbd7938fdd066a532a88681d5bf7fa99264
Viktor Avramenko <viktorio.avramenko@gmail.com>
2015-05-08 18:13:56 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("game1.py")]
+
+cx_Freeze.setup(
+	name="Slither"
+	options={"build_exe":{"packages":["pygame"],"include_files":["apple.png", "snakehead.png"]}},
+
+
+
+
+
+	)
+test
