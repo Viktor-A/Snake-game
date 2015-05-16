85f2d125cb9f2d044965b87bd9507430ad441023
Viktor Avramenko <viktorio.avramenko@gmail.com>
2015-05-16 15:09:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ import cx_Freeze
 executables = [cx_Freeze.Executable("game1.py")]
 
 cx_Freeze.setup(
-	name="Slither"
+	name="Slither",
 	options={"build_exe":{"packages":["pygame"],"include_files":["apple.png", "snakehead.png"]}},
-
-
-
-
-
+	description = "Slither Game Tutorial",
+	executables = executables
 	)
-test
